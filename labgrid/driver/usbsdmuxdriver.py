0521a7e247c1bc5ce046db1b96d5764dc23322c6
Rouven Czerwinski <rouven@czerwinskis.de>
2018-11-23 10:45:24 +0100

    driver/usbsdmuxdriver: implement BootstrapProtocol
    By implementing the BootstrapProtocol, the load function can be used with the
    USBSDMux.
    
    Signed-off-by: Rouven Czerwinski <rouven@czerwinskis.de>

--- labgrid/driver/usbsdmuxdriver.py
+++ labgrid/driver/usbsdmuxdriver.py
@@ -7,11 +7,13 @@ from ..factory import target_factory
 from ..resource.remote import NetworkUSBSDMuxDevice
 from ..resource.udev import USBSDMuxDevice
 from ..step import step
+from ..protocol import BootstrapProtocol
 from .exception import ExecutionError
+from ..util.managedfile import ManagedFile
 
 @target_factory.reg_driver
 @attr.s(cmp=False)
-class USBSDMuxDriver(Driver):
+class USBSDMuxDriver(Driver, BootstrapProtocol):
     """The USBSDMuxDriver uses the usbsdmux tool
     (https://github.com/pengutronix/usbsdmux) to control the USB-SD-Mux
     hardware
@@ -42,3 +44,20 @@ class USBSDMuxDriver(Driver):
             mode.lower()
         ]
         subprocess.check_call(cmd)
+
+    @Driver.check_active
+    @step(args=["filename"])
+    def load(self, filename):
+        if not self.mux.path:
+            raise ExecutionError("SDMux ist not in host mode")
+        mf = ManagedFile(filename, self.mux)
+        mf.sync_to_resource()
+
+        cmd = self.mux.command_prefix + [
+            'cp',
+            mf.get_remote_path(),
+            self.mux.path
+        ]
+
+        subprocess.check_call(cmd)
+
