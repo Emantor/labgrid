992c1acba8a9610a16a69ad0305d1b4b858b2aae
Rouven Czerwinski <r.czerwinski@pengutronix.de>
2021-01-22 19:38:45 +0100

    tests: pyvisa: skip if pyvisa is not importable
    We don't need these tests if pyvisa can't be imported.
    
    Signed-off-by: Rouven Czerwinski <r.czerwinski@pengutronix.de>

--- tests/test_pyvisa.py
+++ tests/test_pyvisa.py
@@ -1,6 +1,12 @@
+from importlib.util import find_spec
+
 from labgrid.resource.pyvisa import PyVISADevice
 from labgrid.driver.pyvisadriver import PyVISADriver
 
+import pytest
+
+pytestmark = pytest.mark.skipif(not find_spec("pyvisa"),
+                              reason="crossbar required")
 
 def test_pyvisa_resource(target):
     PyVISADevice(target, name=None, type='TCPIP', url='127.0.0.1')
